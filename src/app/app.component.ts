a7dcbaf8e313a4ab928f1cf180a3a558533b0a69
Blake Aghili <peopleofksmo@gmail.com>
2020-07-03 19:53:16 -0500

    Connected the AddArticle button. Looping through articles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Article} from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-reddit';
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Drones', 'www.DoD.ru', 43),
+      new Article('Full Stack', 'stack.io', 32),
+      new Article('Half Stack', 'half.org', 11)
+    ];
+  }
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`So we submitted article title ${title.value} from this site: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 1));
+
+    title.value = '';
+    link.value = '';
+
     return false;
   }
 }
